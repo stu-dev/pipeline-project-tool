3a08d20259f1c44dfe4290b5016f7b5d408cbf9f
Stuart Gray <stuartflgray@gmail.com>
2021-11-03 15:47:44 +0000

    comment out tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,17 +3,16 @@ import {expect, test} from '@oclif/test';
 import cmd = require('../src');
 
 describe('pipeline-project-tool', () => {
-  test
-    .stdout()
-    .do(() => cmd.run([]))
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world');
-    });
-
-  test
-    .stdout()
-    .do(() => cmd.run(['--name', 'jeff']))
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff');
-    });
+  // test
+  //   .stdout()
+  //   .do(() => cmd.run([]))
+  //   .it('runs hello', ctx => {
+  //     expect(ctx.stdout).to.contain('hello world');
+  //   });
+  // test
+  //   .stdout()
+  //   .do(() => cmd.run(['--name', 'jeff']))
+  //   .it('runs hello --name jeff', ctx => {
+  //     expect(ctx.stdout).to.contain('hello jeff');
+  //   });
 });
